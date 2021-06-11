19c991acabfdd3805d91020d1e3f7f64cb2220d5
Luca <luca.polese@outlook.com>
2021-06-11 13:48:12 +0200

    Translation completed for buttons.js

---
+++ it/includes/buttons.js
@@ -0,0 +1,13 @@
+runlatex.texts ={
+    "Open in Overleaf": "Apri su Overleaf",
+    "TeXLive.net":      "Esegui su TeXLive.net",
+    "Delete Output":    "Cancella il risultato",
+    "Compiling PDF":    "Compilazione del PDF",
+    // The following not used on learnlatex.org
+    "edit":             "modifica",
+    "copy":             "copia",
+    "Added Code":       "Codice aggiunto",
+    "End Added Code":   "Fine codice aggiunto",
+    "Top Caption":      "Modifica ed esegui questo esempio:"
+}
+
